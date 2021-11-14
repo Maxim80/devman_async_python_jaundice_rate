caced9b00f9c62d3aec3a9b100f2e794374f7b2d
Maxim <maxim19802104@gmail.com>
2021-11-14 16:22:39 +0300

    Server sends JSON in response to requests.

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ import logging
 import time
 
 
-TEST_ARTICLES = [
-        'https://inosmi.ru/economic/20211105/250847958.html',
-        'https://inosmi.ru/economic/20211104/250846376.html',
-        'https://inosmi.ru/social/20211110/250870936.html',
-        'https://inosmi.ru/social/20211110/250867022.html',
-        'https://inosmi.ru/social/20211110/250865347.html',
-        'https://inosmi.ru/not/exist.html',
-        'https://lenta.ru/brief/2021/08/26/afg_terror/',
-    ]
-
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('root')
 
@@ -45,12 +34,12 @@ def get_adapter(url):
     return adapter
 
 
-def packing_into_dict(title, status, score, words_count):
+def packing_into_dict(url, status, score, words_count):
     return {
-        'Заголовок': title,
-        'Статус': status,
-        'Рейтинг': score,
-        'Слов в статье': words_count,
+        'url': url,
+        'status': status,
+        'score': score,
+        'words_count': words_count,
     }
 
 
@@ -72,7 +61,7 @@ async def fetch(session, url):
         return await response.text()
 
 
-async def process_article(session, morph, charged_words, url, title, processing_results):
+async def process_article(session, morph, charged_words, url, processing_results):
     try:
         async with async_timeout.timeout(3):
             html = await fetch(session, url)
@@ -80,13 +69,13 @@ async def process_article(session, morph, charged_words, url, title, processing_
         adapter = get_adapter(url)
 
     except asyncio.exceptions.TimeoutError:
-        processing_results.append(packing_into_dict(title, 'TIMEOUT', None, None))
+        processing_results.append(packing_into_dict(url, 'TIMEOUT', None, None))
 
     except aiohttp.client_exceptions.ClientResponseError:
-        processing_results.append(packing_into_dict(title, 'FETCH_ERROR', None, None))
+        processing_results.append(packing_into_dict(url, 'FETCH_ERROR', None, None))
 
     except KeyError:
-        processing_results.append(packing_into_dict(title, 'PARSING_ERROR', None, None))
+        processing_results.append(packing_into_dict(url, 'PARSING_ERROR', None, None))
 
     else:
         article_text = adapter(html, plaintext=True)
@@ -95,28 +84,31 @@ async def process_article(session, morph, charged_words, url, title, processing_
             score = calculate_jaundice_rate(separated_words, charged_words)
 
         words_count = len(separated_words)
-        processing_results.append(packing_into_dict(title, 'OK', score, words_count))
+        processing_results.append(packing_into_dict(url, 'OK', score, words_count))
 
 
 
-async def main():
+async def main(test_articles):
     morph = pymorphy2.MorphAnalyzer()
     charged_words = await get_charged_worlds()
     processing_results = []
     async with aiohttp.ClientSession() as session:
         async with create_task_group() as task_group:
-            for url_article in TEST_ARTICLES:
-                title = f'URL: {url_article}'
-                await task_group.spawn(process_article, session, morph, charged_words, url_article, title, processing_results)
+            for url_article in test_articles:
+                await task_group.spawn(process_article, session, morph, charged_words, url_article, processing_results)
 
-    for result in processing_results:
-        print('')
-        print('Заголовок: ', result['Заголовок'])
-        print('Статус: ', result['Статус'])
-        print('Рейтинг: ', result['Рейтинг'])
-        print('Слов в статье: ', result['Слов в статье'])
-        print('')
+    print(*processing_results)
+
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    test_articles = [
+            'https://inosmi.ru/economic/20211105/250847958.html',
+            'https://inosmi.ru/economic/20211104/250846376.html',
+            'https://inosmi.ru/social/20211110/250870936.html',
+            'https://inosmi.ru/social/20211110/250867022.html',
+            'https://inosmi.ru/social/20211110/250865347.html',
+            'https://inosmi.ru/not/exist.html',
+            'https://lenta.ru/brief/2021/08/26/afg_terror/',
+        ]
+    asyncio.run(main(test_articles))
--- server.py
+++ server.py
@@ -1,10 +1,40 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
+from anyio import create_task_group
+from main import get_charged_worlds, process_article
+import pymorphy2
+
+
+class NoDataInQuery(Exception):
+    pass
+
+
+def pack_query_to_list(query):
+    if not query:
+        raise NoDataInQuery
+    return query.split(',')
+
+
+async def process_article_wrapper(urls):
+    morph = pymorphy2.MorphAnalyzer()
+    charged_words = await get_charged_worlds()
+    processing_results = []
+    async with ClientSession() as session:
+        async with create_task_group() as task_group:
+            for url in urls:
+                await task_group.spawn(process_article, session, morph, charged_words, url, processing_results)
+
+    return processing_results
 
 
 async def handle(request):
-    name = request.match_info.get('name', 'Anonymous')
-    text = 'Hello, ' + name
-    return web.Response(text=text)
+    try:
+        query = request.query.get('urls')
+        query_values = pack_query_to_list(query)
+    except (KeyError, NoDataInQuery):
+        return web.json_response({'error': 'No data in query.'})
+
+    json_data = await process_article_wrapper(query_values)
+    return web.json_response(json_data)
 
 
 app = web.Application()
@@ -15,3 +45,8 @@ app.add_routes([
 
 if __name__ == '__main__':
     web.run_app(app)
+
+"""
+http://0.0.0.0:8080?urls=https://inosmi.ru/economic/20211105/250847958.html,https://inosmi.ru/economic/20211104/250846376.html,https://inosmi.ru/social/20211110/250870936.html,https://inosmi.ru/social/20211110/250867022.html,https://inosmi.ru/social/20211110/250865347.html,https://inosmi.ru/not/exist.html,https://lenta.ru/brief/2021/08/26/afg_terror/
+
+"""
